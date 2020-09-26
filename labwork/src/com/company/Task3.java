47d02dca80d567884273c7c177ee8f99895ba129
MikhasIa <55757292+MikhasIa@users.noreply.github.com>
2020-09-26 09:49:09 +0300

    Add files via upload

---
+++ labwork/src/com/company/Task3.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Task3
+{
+
+    public static void main(String[] args) {
+        int a=2019;
+        int b=98;
+        int c=a-b;
+        System.out.println("Мой юный друг\nСейчас "+ a +". Наш университет основан "+ b +" лет назад.\n"+c);
+
+    }
+}
